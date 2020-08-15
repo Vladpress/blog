d1067f2ad19404973336506e85eaad2ef5ead9a3
Vladpress <pixel22yuliya@gmail.com>
2020-08-16 02:46:49 +0300

    changed logic a little, added linter, proptypes and ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,53 @@
+import  React, { useState, useEffect } from 'react';
 import * as styles from '../styles/styled/posts';
 import Link from 'next/link';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
-import MainLayout from '../components/MainLayout'
+import MainLayout from '../components/MainLayout';
 
-const { H1, PostList, Title, Body, Post } = styles; 
+const { H1, PostList, Title, Body, Post } = styles;
 
-export default function Posts({ posts }) {
-  
+export default function Posts({ posts: serverPosts }) {
+  const [posts, setPosts] = useState(serverPosts);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('https://simple-blog-api.crew.red/posts');
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    if (!serverPosts) {
+      load()
+    }
+  }, []);
+
+  if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading ...</p>
+      </MainLayout>         
+    )
+  }
+
   return (
     <MainLayout>
+      <Head>
+        <title>Posts Page | Next Course</title>
+      </Head>
       <H1>Posts
         <Link href={'/new'}>
-                  <a>
-                    +
-                  </a>
-                </Link>
+          <a>
+            +
+          </a>
+        </Link>
       </H1>
-      <PostList>       
+      <PostList>
         {posts.map((post, index) => {
-        
-          return post.body && post.body.length > 100 ?        
-            (      
+
+          return post.body && post.body.length > 100 ?
+            (
               <Post key={index}>
                 <Link href={`/post/[id]`} as={`/post/${post.id}`}>
                   <a>
@@ -32,17 +58,24 @@ export default function Posts({ posts }) {
                 </Link>
               </Post>
             ) : ""
-          })}     
+        })}
       </PostList>
     </MainLayout>
   )
 }
 
-Posts.getInitialProps = async () => {
-  const response = await fetch('https://simple-blog-api.crew.red/posts');
-  const posts = await response.json();  
+Posts.getInitialProps = async ({ req }) => {
   
+  if(!req) return { posts:null }
+
+  const response = await fetch('https://simple-blog-api.crew.red/posts');
+  const posts = await response.json();
+
   return {
     posts
   }
 } 
+
+Posts.propTypes = {
+  posts:  PropTypes.arrayOf(PropTypes.object)
+};
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,11 +1,31 @@
-import {useState, useEffect} from 'react'
-import {useRouter} from 'next/router'
-import Link from 'next/link'
+import React, {useState, useEffect} from 'react';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import PropTypes from 'prop-types';
+import MainLayout from '../../components/MainLayout';
 
-import MainLayout from '../../components/MainLayout'
+export default function Post({ post: serverPost }) {
+  const [post, setPost] = useState(serverPost)
+  const router = useRouter()
 
-export default function Post({ post }) {
-  
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`https://simple-blog-api.crew.red/posts/${router.query.id}`);
+      const data = await response.json();
+      setPost(data);
+    }
+
+    if (!serverPost) {
+      load()
+    }
+  }, [])
+
+  if (!post) {
+    return <MainLayout>
+      <p>Loading ...</p>
+    </MainLayout>
+  }
+
   return (   
     <MainLayout>
       <h1>{post.title}</h1>
@@ -16,11 +36,20 @@ export default function Post({ post }) {
   )
 }
 
-Post.getInitialProps = async ( ctx ) => {
-  const response = await fetch(`https://simple-blog-api.crew.red/posts/${ctx.query.id}`);
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) return {post: null}
+  console.log(typeof query.id);
+  const response = await fetch(`https://simple-blog-api.crew.red/posts/${ query.id }`);
   const post = await response.json();
 
   return {
     post
   }
 }
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+  })
+};
